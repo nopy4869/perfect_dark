1a2995ca762900c26eceeb6d0ad31e5c980c9420
Ian McWhorter <nopy4869@gmail.com>
2024-09-08 03:31:04 -0500

    made some minor tweaks

--- tools/mkresource.sh
+++ tools/mkresource.sh
@@ -9,13 +9,13 @@ BEGIN
     BLOCK "040904E4"
     BEGIN
       VALUE "CompanyName", "Perfect Dark decompilation project"
-      VALUE "FileDescription", "The unofficial Win32 port of Perfect Dark $2-$1"
+      VALUE "FileDescription", "The unofficial Win32 port of Perfect Dark $3 $2 $1"
       VALUE "FileVersion", "$1"
-      VALUE "InternalName", "perfectDark-Win32-$2-$1"
+      VALUE "InternalName", "perfectDark-Win32-$3-$2-$1"
       VALUE "LegalCopyright", "(C) 2022 Ryan Dwyer, (C) 2023 fgsfdsfgs"
-      VALUE "OriginalFilename", "$3"
+      VALUE "OriginalFilename", "$4"
       VALUE "ProductName", "Perfect Dark Win32"
-      VALUE "ProductVersion", "$2-$1"
+      VALUE "ProductVersion", "$3-$2-$1"
     END
   END
 
