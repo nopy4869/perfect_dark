3bf449aef24f594f11d19d3333dbb755b9f02581
Ian McWhorter <nopy4869@gmail.com>
2024-09-08 03:19:02 -0500

    Fixed tools being in the wrong place and changed some text

--- tools/mkresource.sh
+++ tools/mkresource.sh
@@ -1,5 +1,3 @@
-echo $1
-echo $2
 tee dist/windows/icon.rc <<EOF
 100 ICON "./icon.ico"
 1 VERSIONINFO
@@ -11,12 +9,12 @@ BEGIN
     BLOCK "040904E4"
     BEGIN
       VALUE "CompanyName", "Perfect Dark decompilation project"
-      VALUE "FileDescription", "The unofficial PC port of Perfect Dark $2-$1"
+      VALUE "FileDescription", "The unofficial Win32 port of Perfect Dark $2-$1"
       VALUE "FileVersion", "$1"
-      VALUE "InternalName", "perfectDark-pc-$2-$1"
+      VALUE "InternalName", "perfectDark-Win32-$2-$1"
       VALUE "LegalCopyright", "(C) 2022 Ryan Dwyer, (C) 2023 fgsfdsfgs"
       VALUE "OriginalFilename", "pd.exe"
-      VALUE "ProductName", "Perfect Dark PC"
+      VALUE "ProductName", "Perfect Dark Win32"
       VALUE "ProductVersion", "$2-$1"
     END
   END
